53408ae8e28f70226fe31382aacac31b93d2c0c6
Vedanta SP <vedant.vasu1111@gmail.com>
2022-11-01 18:17:55 +0530

    added files from mac

---
+++ pages/index.js
@@ -0,0 +1,50 @@
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBBtn
+} from "mdb-react-ui-kit";
+import Link from "next/link";
+const axios = require("axios").default;
+
+const index = ({ heros }) => {
+  return (
+    <div className="container">
+      <h1 className="diplay-2">Superhero Identity manager</h1>
+      <div>
+        {heros.map((hero) => {
+          return (
+            <MDBCard
+              className="border boder-2 my-2"
+              style={{ maxWidth: "22rem" }}
+            >
+              <MDBCardBody>
+                <MDBCardTitle>{hero.superHero}</MDBCardTitle>
+                <MDBCardText>Reveal Identity</MDBCardText>
+                <Link href={`/${hero._id}`}>
+                  <MDBBtn className="mx-2">View Hero</MDBBtn>
+                </Link>
+                <Link href={`/${hero._id}/edit`}>
+                  <MDBBtn>Edit Hero</MDBBtn>
+                </Link>
+              </MDBCardBody>
+            </MDBCard>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export async function getServerSideProps(context) {
+  const res = await axios("http://localhost:3000/api/hero");
+  // console.log(res.data.hero);
+  const { hero } = res.data;
+  //console.log(hero);
+  return {
+    props: { heros: hero }
+  };
+}
+
+export default index;
